8494f8f1c07e80ca7a223916192b95c9c00b3b75
Frank J. Rachal <francisrachal@gmail.com>
2026-05-16 14:46:15 -0400

    fix terraform S3 website configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,29 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
 resource "random_id" "bucket_suffix" {
   byte_length = 4
 }
 
-resource "aws_s3_object" "index" {
-  bucket       = aws_s3_bucket.website.id
-  key          = "index.html"
-  source       = "${path.module}/../website/index.html"
-  content_type = "text/html"
-  source_hash  = filemd5("${path.module}/../website/index.html")
+resource "aws_s3_bucket" "website" {
+  bucket = "frank-s3-cicd-pipeline-${random_id.bucket_suffix.hex}"
 }
 
 resource "aws_s3_bucket_public_access_block" "website" {
@@ -19,6 +35,8 @@ resource "aws_s3_bucket_public_access_block" "website" {
   restrict_public_buckets = false
 }
 
+resource "aws_s3_bucket_website_configuration" "website" {
+  bucket = aws_s3_bucket.website.id
 
   index_document {
     suffix = "index.html"
@@ -51,5 +69,9 @@ resource "aws_s3_object" "index" {
   key          = "index.html"
   source       = "${path.module}/../website/index.html"
   content_type = "text/html"
-  etag         = filemd5("${path.module}/../website/index.html")
+  source_hash  = filemd5("${path.module}/../website/index.html")
+}
+
+output "website_url" {
+  value = aws_s3_bucket_website_configuration.website.website_endpoint
 }
